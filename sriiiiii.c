6d67aa5175a0aed6c1a5bc869e09ae21bdf71ff0
Sabhariaswath <35764741+Sabhariaswath@users.noreply.github.com>
2018-01-31 16:44:20 +0530

    sriiiiii
    steel

---
+++ sriiiiii.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number",a);
+scanf("%d",&a);
+if(a%2==0)
+printf("The number is even");
+else
+{
+printf("the number is odd");
+}
+return 0;
+}
