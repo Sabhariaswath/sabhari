6ff802418145c98bd9a08d5bc8c905d885375f73
Sabhariaswath <35764741+Sabhariaswath@users.noreply.github.com>
2018-03-14 16:15:26 +0530

    Create sri56.c

---
+++ sri56.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[10];
+int i;
+scanf("%s",a);
+if((a[i]>='a')&&(a[i]<='z')&&(a[i]>='0')&&(a[i]<='9'))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
